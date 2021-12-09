68ebe76bc93dff28004eae642eb2645f85762ac1
Jorge Malo <jorge.malo@getbuilt.com>
2021-12-08 23:32:46 -0500

    Added form into nav app

--- nav-app/browser-app.js
+++ nav-app/browser-app.js
@@ -4,3 +4,8 @@ const links = document.querySelector('.links')
 navToggle.addEventListener('click', function () {
   links.classList.toggle('show-links')
 })
+
+const clearFile = (type) => {
+  const file = type === 'single' ? document.getElementById('myFile') : document.getElementById('myFiles');
+  file.value = "";
+};
